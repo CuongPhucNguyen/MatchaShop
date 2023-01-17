084625e97d269f083161774b4b3a10624f1b09f5
Hieu <vip12345k@gmail.com>
2023-01-17 20:15:30 +0700

    added services

--- app/src/main/java/com/example/matchashop/models/UserModel.java
+++ app/src/main/java/com/example/matchashop/models/UserModel.java
@@ -8,4 +8,45 @@ public class UserModel {
     private String userImgURL;
     private List<OrderModel> orders;
 
+    public UserModel(String username, String email, String userImgURL, List<OrderModel> orders) {
+        Username = username;
+        Email = email;
+        this.userImgURL = userImgURL;
+        this.orders = orders;
+    }
+
+    public UserModel() {
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getUserImgURL() {
+        return userImgURL;
+    }
+
+    public void setUserImgURL(String userImgURL) {
+        this.userImgURL = userImgURL;
+    }
+
+    public List<OrderModel> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<OrderModel> orders) {
+        this.orders = orders;
+    }
 }
