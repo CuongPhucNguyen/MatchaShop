9b815ea9dd59e104f6e654d4fa187c3780a87e1e
An Do <an.dtruongan@gmail.com>
2023-01-17 20:46:51 +0700

    fix user ui

--- app/src/main/java/com/example/matchashop/SignInLogin/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/matchashop/SignInLogin/ui/dashboard/DashboardFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.matchashop.R;
 import com.example.matchashop.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
@@ -24,8 +25,9 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        this.getActivity().setContentView(R.layout.fragment_dashboard);
+
         return root;
     }
 
